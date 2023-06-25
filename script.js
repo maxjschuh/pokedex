ae1d39d064bd41265183c766d5fc5d0784e208f7
maxjschuh <maximilianschuh@gmx.de>
2023-06-25 02:39:58 +0200

    next steps in designing the detail view

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let pokedexBase = [];
 let pokedexSpecies = [];
 
+//---------------------------------------
+// init
+//---------------------------------------
+
 async function init() {
 
     await fetchData(1, 21);
@@ -16,6 +20,65 @@ async function loadMore() {
     render();
 }
 
+//---------------------------------------
+//test area
+//---------------------------------------
+
+// str = str.replace(/[\u00A0\u1680​\u180e\u2000-\u2009\u200a​\u200b​\u202f\u205f​\u3000]/g,'');
+
+
+async function init2() {
+
+    await fetchData(1, 21);
+    await fetchDataPokemonSpecies(1, 21);
+    renderDetailView(1);
+}
+
+function renderDetailView(i) {
+    let container = document.getElementById('detail-view');
+
+    container.innerHTML = templateDetailView(i);
+}
+
+function templateDetailView(i) {
+
+    const pokemon = pokedexBase[i];
+
+    const name = pokemon['forms'][0]['name'];
+    const imgUrl = pokemon['sprites']['other']['home']['front_default'];
+    const pokedexBaseId = '#' + convertToTripleDigits(i);
+    const flavorText = pokedexSpecies[i]['flavor_text_entries'][0]['flavor_text'];
+
+    let html = /*html*/ `
+
+        <h2>${name} ${pokedexBaseId}</h2>
+        <img src="${imgUrl}" alt="${name}">
+
+        <div>
+        <h3>About</h3>
+        <p>${flavorText}</p>
+        </div>
+
+        <div>
+            <h3>Status</h3>
+        </div>
+
+        <div>
+            <h3>Moves</h3>
+        </div>
+
+        <div>
+            <h3>Evolutions</h3>
+        </div>
+    `;
+
+    return html;
+}
+
+
+//---------------------------------------
+// fetch data
+//---------------------------------------
 async function fetchData(start, end) {
 
     for (let i = start; i < end; i++) {
@@ -23,9 +86,9 @@ async function fetchData(start, end) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
 
         let response = await fetch(url);
-        let responseJson = await response.json();  
-        
-        pokedexBase.push(responseJson);      
+        let responseJson = await response.json();
+
+        pokedexBase.push(responseJson);
     }
 }
 
@@ -39,9 +102,12 @@ async function fetchDataPokemonSpecies(start, end) {
         let responseJson = await response.json();
 
         pokedexSpecies.push(responseJson);
-    }    
+    }
 }
 
+//---------------------------------------
+//render cards
+//---------------------------------------
 function render() {
 
     let container = document.getElementById('main');
@@ -73,32 +139,7 @@ function templatepokedexBaseCard(i) {
     return html;
 }
 
-function renderDetailView(i) {
-    let container = document.getElementById('detail-view');
 
-    container.innerHTML = templateDetailView(i);
-}
-
-function templateDetailView(i) {
-
-    const pokemon = pokedexBase[i];
-
-    const name = pokemon['forms'][0]['name'];
-    const imgUrl = pokemon['sprites']['other']['home']['front_default'];
-    const pokedexBaseId = '#' + convertToTripleDigits(i);
-    const flavorText = pokedexSpecies[i]['flavor_text_entries'][0]['flavor_text'];
-
-    let html = /*html*/ `
-
-        <h2>${name} ${pokedexBaseId}</h2>
-        <img src="${imgUrl}" alt="${name}">
-
-        <h3>About</h3>
-        <p>${flavorText}</p>
-    `;
-
-    return html;
-}
 
 function convertToTripleDigits(i) {
 
